c8221b74659d596b88166cee87091cd0e0d5d68b
Nicolò Santilio <n.santilio97@gmail.com>
2024-08-21 17:22:10 +0200

    Update handler.go

--- redirector/handler.go
+++ redirector/handler.go
@@ -31,7 +31,7 @@ func Init(config *config.Config) {
 		log.Fatal("Failed to connect to Redis")
 		panic("Failed to connect to Redis")
 	}
-	uri = config.Redirector.GetURI()
+	uri = config.Redirector.Public_url
 	log.Println("Connected to Redis on " + config.Redis.GetAddr())
 
 }
